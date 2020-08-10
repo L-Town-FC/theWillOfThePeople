f773f78e9030ef60d1d5c70e314f64b53ea6e50f
Colin Krohto <ColinKrohto@gmail.com>
2020-08-10 16:10:26 -0400

    rewrote ceelo start and ceelo join

--- commands/ceelo.js
+++ commands/ceelo.js
@@ -1,3 +1,6 @@
+const { type } = require('os')
+const { join } = require('path')
+
 module.exports = {
     name: 'ceelo',
     description: 'lets you play ceelo with others',
@@ -7,28 +10,29 @@ module.exports = {
         const Discord = require('discord.js')
         const embed = require('./Functions/embed_functions')
         var command = args[1] || 'default'
+        ceelo = JSON.parse(fs.readFileSync('./JSON/ceelo.json', 'utf-8'))
 
         switch(command){
             case 'start':
                 //!ceelo start [bet] Initiates game with specified bet
                 //creates timer for others to join and then closes so people can't join midgame
-                Ceelo_Start(message, args, master)
+                Ceelo_Start(message, args, ceelo, master)
             break;
             case 'join':
                 //lets people join the game before it closes
-                Ceelo_Join(message, master)
+                Ceelo_Join(message, ceelo, master)
             break;
             case 'leave':
                 //lets people leave the game before it closes
-                Ceelo_Leave(message, args, ceelo_players, master)
+                Ceelo_Leave(message, args, ceelo, master)
             break;
             case 'roll':
                 //lets you roll the dice
-                Ceelo_Roll(message, args, ceelo_players, master)
+                Ceelo_Roll(message, args, ceelo, master)
             break;
             case 'wash':
                 //lets you roll the dice but they don't count
-                Ceelo_Wash(message, args, ceelo_players, master)
+                Ceelo_Wash(message, args, ceelo, master)
             break;
             case 'basics':
                 //Shows the the hand rankings
@@ -47,59 +51,124 @@ module.exports = {
     }
 }
 
-function Ceelo_Start(message, args, master){
+function Ceelo_Start(message, args, ceelo, master){
+    const fs = require('fs')
     const min_bet = 15
     var time = 15
-    
-    if(typeof(ceelo_players) !== 'undefined'){
-        message.channel.send('Game is already ongoing')
-    }else if(isNaN(args[2]) == true){
-        message.channel.send('Please give a valid bet amount')
-    }else if(args[2] < min_bet){
-        message.channel.send(`The minimum bet is ${min_bet} gbp`)
-    }else if(parseFloat(args[2]) > master[message.author.id].gbp){
-        message.channel.send("You don't have enough gbp for that bet")
+    var games = Object.keys(ceelo.games).length
+    var bet = args[2]
+    for(var j in ceelo.games){
+        //checks if there are any games that have been created but not started and would therefore be joinable
+        if(ceelo.games[j].game_start == false){
+            var ongoing = true
+        }
+    }
+    if(isNaN(bet) == true || parseFloat(bet) < min_bet){
+        message.channel.send(`Please give a valid bet of ${min_bet} gbp or greater`)
+    }else if(master[message.author.id].gbp < parseFloat(bet)){
+        message.channel.send(`You don't have enough gbp for that bet`)
     }else{
-        ceelo_players = []
-        ceelo_bet = args[2]
-        ceelo_players.push([message.author.id, 0])
-        game_join = true
-        message.channel.send(`Players have ${time} seconds to join the game`)
-        message.channel.send(`${master[message.author.id].name} joined the game`)
-        setTimeout(function(){
-            if(ceelo_players.length > 1){
-                game_start = true
+        if(typeof(ongoing) == 'undefined'){
+            //if no joinable games exist it means that this variable is undefined
+            //A joinable game is then created
+            message.channel.send(`You have ${time} seconds to join the game`)
+            if(games == 0){
+                var i = 1
+                ceelo.games[i] = {
+                    "participants" : [[message.author.id ,0 , 0]],
+                    "bet": parseFloat(bet),
+                    "game_start": false,
+                    "game_over": false,
+                }
             }else{
-                message.channel.send('Not enough people. Game cancelled')
-                delete ceelo_players
-                delete game_join
+                for(var i in ceelo.games){
+                    if(ceelo.games[i].participants.includes(message.author.id)){
+                        //var inGame = true
+                        //uncomment this out so you can only join 1 game at a time
+                    }
+                }
+                if(typeof(inGame) === 'undefined'){
+                    i = parseInt(i) + 1
+                    ceelo.games[i] = {
+                        "participants" : [[message.author.id ,0 , 0]],
+                        "bet": parseFloat(bet),
+                        "game_start": false,
+                        "game_over": false
+                    }
+                }
             }
-        }, time * 1000)
+            if(typeof(i) !== 'undefined'){
+                setTimeout(function(){
+                    var ceelo2 = JSON.parse(fs.readFileSync('./JSON/ceelo.json', 'utf-8'))
+                    if(ceelo2.games[i].participants.length > 1){
+                        ceelo2.games[i].game_start = true
+                    }else{
+                        message.channel.send('Not enough players. Game cancelled')
+                        delete ceelo2.games[i]
+                    }
+                    fs.writeFileSync ("./JSON/ceelo.json", JSON.stringify(ceelo2, null, 2), function(err) {
+                        if (err) throw err;
+                        console.log('complete');
+                        }
+                    );
+                },time * 1000)
+            }
+        }else{
+            //A joinable game does exist so there is no need to create a new one
+            message.channel.send('There is already a joinable game')
+        }
+        fs.writeFileSync ("./JSON/ceelo.json", JSON.stringify(ceelo, null, 2), function(err) {
+            if (err) throw err;
+            console.log('complete');
+            }
+        );
     }
 }
 
-function Ceelo_Join(message, master){
-    if(typeof(game_join) == 'undefined'){
-        message.channel.send('There is no game to join')
-    }else if(typeof(game_start) !== 'undefined'){
-        message.channel.send("You can't join because the game has already started")
-    }else if(master[message.author.id].gbp < ceelo_bet){
-        message.channel.send(`You must have atleast ${ceelo_bet} gbp to play this hand`)
-    }else if(2 !== 2){
-        //change it so same player can't join twice
+function Ceelo_Join(message, ceelo, master){
+    const fs = require('fs')
+    var ceelo = JSON.parse(fs.readFileSync('./JSON/ceelo.json', 'utf-8'))
+    for(var i in ceelo.games){
+        if(typeof(ceelo.games[i]) !== 'undefined'){    
+            for(var j in ceelo.games[i].participants){
+                if(ceelo.games[i].participants[j][0] == message.author.id){
+                    //var inGame = true
+                    //uncomment this out later so same person can't be in multiple games
+                }
+            }
+            if(ceelo.games[i].game_start == false){
+                var joinable = true
+                var join_index = i
+            }
+        }
+    }
+    if(typeof(joinable) !== 'undefined'){
+        if(typeof(inGame) == 'undefined'){
+            //If you aren't in a game currently and there a joinable game exists you are here
+            if(master[message.author.id].gbp >= ceelo.games[join_index].bet){
+                ceelo.games[join_index].participants.push([message.author.id ,0 , 0])
+                fs.writeFileSync ("./JSON/ceelo.json", JSON.stringify(ceelo, null, 2), function(err) {
+                    if (err) throw err;
+                    console.log('complete');
+                    }
+                );
+            }else{
+                message.channel.send(`You must have at least ${ceelo.games[join_index].bet} gbp to join`)
+            }
+        }else{
+            message.channel.send('You are already in a game')
+        }   
     }else{
-        console.log(ceelo_players)
-        ceelo_players.push([message.author.id, 0])
-        console.log(ceelo_players)
-        message.channel.send(`${master[message.author.id].name} joined the game`)
+        message.channel.send('No joinable game')
     }
-}
-
-function Ceelo_Leave(message, args, ceelo_players, master){
     
 }
 
-function Ceelo_Roll(message, args, ceelo_players, master){
+function Ceelo_Leave(message, args, ceelo, master){
+    
+}
+
+function Ceelo_Roll(message, args, ceelo, master){
     //Starts by saying whos turn it is to roll
     //they have 15 seconds to roll or wash
     //once they roll, change the second number in the pair to 1
@@ -108,7 +177,7 @@ function Ceelo_Roll(message, args, ceelo_players, master){
     //once winner has been decided, clear ceelo_players and game_start
 }
 
-function Ceelo_Wash(message, args, ceelo_players, master){
+function Ceelo_Wash(message, args, ceelo, master){
     
 }
 
