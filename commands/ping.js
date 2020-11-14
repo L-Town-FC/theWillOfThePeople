1e2b902d68992dc24df3427ad551e2d8a5245255
Colin Krohto <ColinKrohto@gmail.com>
2020-11-14 13:59:57 -0500

    finished poll.js logic

--- commands/ping.js
+++ commands/ping.js
@@ -3,35 +3,29 @@ module.exports = {
     description: 'says pong',
     execute(message){
         //message.channel.send('pong');
-        var temp_data = [[1,2,3], [2,3],[2,1],[3],[1,3],[2],[3],[2],[1,2,3]]
-        var count = [[1,0],[2,0],[3,0]]
+        
+        var temp_data = [[1,2,3,4],[2,3,4],[4,2],[2,3,1],[4,2,1],[3,2,4],[1,2,4],[4,2,3],[1,2],[4,2],[4,1],[2,1],[3,2,1],[4,3,1],[2,4,1],[1,4,3]]
+        var count = [[1,0],[2,0],[3,0],[4,0]]
         var run_off = true
-        var removed_choices = []
+        var removed_choice
         var removed_index = 0
-        var already_counted = false
-        var to_be_removed = []
+        var already_voted = false
+        
         while (run_off == true){
             for(var i = 0; i < temp_data.length; i++){
                 for(var j = 0; j < temp_data[i].length;j++){
-                    if(removed_choices.includes(temp_data[i][j]) == false && already_counted == false){
-                        count[temp_data[i][0] - 1][1] += 1
-                        already_counted = true
-                    }
-                    if(already_counted == false){
-                        to_be_removed.push(i)
-                    }
+                    if(already_voted == false){
+                        count[temp_data[i][0] - 1][1] += 1  
+                        already_voted = true
+                    }  
                 }
-                already_counted = false
+                already_voted = false
             }
-            for(var i in to_be_removed){
-                temp_data.splice(i,1)
-            }
-            //console.log(count)
-            //console.log(removed_choices)
+
+            console.log(count)
             var winner = 0 //0 = none, 1 = winner>50%, 2 = winner = 50% (need to check for tie)
             var winning_choice = -1
             for(var i = 0; i < count.length; i++){
-                console.log(count[i])
                 if(count[i][1]/temp_data.length > 0.5){
                     var winner = 1
                     winning_choice = i
@@ -44,7 +38,6 @@ module.exports = {
                     }
                 }
             }
-
             if(winner == 1){
                 message.channel.send(`${count[winning_choice][0]} was the winner`)
                 run_off = false
@@ -58,13 +51,22 @@ module.exports = {
                 }
             }else{
                 //run off
-                //run_off = false
                 message.channel.send('Run Off')
                 var sorted_array = count.sort(function(a,b){return a[1] - b[1]})
-                removed_choices.push(sorted_array[removed_index][0])
+                removed_choice = sorted_array[removed_index][0]
                 removed_index++
-                for(var i = 0;i < count.length; i++){
-                    count[i] = [i + 1, 0]
+                for(var i = temp_data.length - 1; i > -1; i--){
+                    for(var j = temp_data[i].length - 1; j > -1; j--){
+                        if(temp_data[i][j] == removed_choice){
+                            temp_data[i].splice(j,1)
+                        }
+                    }
+                    if(temp_data[i] == '[]'){
+                        temp_data.splice(i,1)
+                    }
+                }
+                for(var i = 0; i < count.length; i++){
+                    count[i] = [i + 1,0]
                 }
             }
         }
--- commands/poll.js
+++ commands/poll.js
@@ -91,7 +91,7 @@ function Poll_Create(message, args, polls){
             message.channel.send(poll_embed)
 
             setTimeout(function(){
-                Vote_Count(message, polls)
+                Vote_Count(message, polls, new_id)
                 delete polls[new_id]
             },time)
         }else{
@@ -109,7 +109,7 @@ function Poll_Vote(message, args, polls){
     //!poll vote [poll number] [choice 1] [choice 2] ...
     if(Object.keys(polls).includes(args[2]) == true){
         var index = args[2]
-        if(polls[index].voters.includes(message.author.id) == 1/*false*/){
+        if(polls[index].voters.includes(message.author.id) !== 1/*false*/){
             var choices = []
             var problems = false
             for(var i = 3; i < args.length; i++){
@@ -129,6 +129,7 @@ function Poll_Vote(message, args, polls){
             if(problems == false){
                 polls[index].voters.push(message.author.id)
                 polls[index].votes.push(choices)
+                message.channel.send('Your vote was successful')
             }else{
                 message.channel.send('You either tried to put down to many choices, voted the same option more than once, or had invalid choices')
             }
@@ -187,10 +188,76 @@ function Time_calc(args){
     return parseInt(amount) * multiplier * 1000
 }
 
-function Vote_Count(message, polls){
-    var temp_data = [[1,2,3], [2,3],[2,1],[3],[1,3],[2],[3],[2]]
-    var count = [0,0,0]
-    for(var i = 0; i < temp_data; i++){
+function Vote_Count(message, polls, poll_index){
+    //var temp_data = [[1,2,3,4],[2,3,4],[4,2],[2,3,1],[4,2,1],[3,2,4],[1,2,4],[4,2,3],[1,2],[4,2],[4,1],[2,1],[3,2,1],[4,3,1],[2,4,1],[1,4,3]]
+    //var count = [[1,0],[2,0],[3,0],[4,0]]
+    var temp_data = polls[poll_index].votes
+    var count = []
+    for(var i = 0; i < polls[poll_index].options.length; i++){
+        count[i] = [i+1,0]
+    }
+    var run_off = true
+    var removed_choice
+    var removed_index = 0
+    var already_voted = false
+    
+    while (run_off == true){
+        for(var i = 0; i < temp_data.length; i++){
+            for(var j = 0; j < temp_data[i].length;j++){
+                if(already_voted == false){
+                    count[temp_data[i][0] - 1][1] += 1  
+                    already_voted = true
+                }  
+            }
+            already_voted = false
+        }
 
+        console.log(count)
+        var winner = 0 //0 = none, 1 = winner>50%, 2 = winner = 50% (need to check for tie)
+        var winning_choice = -1
+        for(var i = 0; i < count.length; i++){
+            if(count[i][1]/temp_data.length > 0.5){
+                var winner = 1
+                winning_choice = i
+            }else if(count[i][1]/temp_data.length == 0.5){
+                var winner = 2
+                if(winning_choice !== -1){
+                    winning_choice = 99
+                }else{
+                    winning_choice = i
+                }
+            }
+        }
+        if(winner == 1){
+            message.channel.send(`${count[winning_choice][0]} was the winner`)
+            run_off = false
+        }else if(winner == 2){
+            if(winning_choice == 99){
+                message.channel.send(`There is a tie`)
+                run_off = false
+            }else{
+                message.channel.send(`${count[winning_choice][0]} was the winner`)
+                run_off = false
+            }
+        }else{
+            //run off
+            message.channel.send('Run Off')
+            var sorted_array = count.sort(function(a,b){return a[1] - b[1]})
+            removed_choice = sorted_array[removed_index][0]
+            removed_index++
+            for(var i = temp_data.length - 1; i > -1; i--){
+                for(var j = temp_data[i].length - 1; j > -1; j--){
+                    if(temp_data[i][j] == removed_choice){
+                        temp_data[i].splice(j,1)
+                    }
+                }
+                if(temp_data[i] == '[]'){
+                    temp_data.splice(i,1)
+                }
+            }
+            for(var i = 0; i < count.length; i++){
+                count[i] = [i + 1,0]
+            }
+        }
     }
 }
