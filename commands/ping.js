04ccf7386c7303c7bbabb87a614c41c0c2599d80
Colin Krohto <ColinKrohto@gmail.com>
2020-11-24 12:39:46 -0500

    patch

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,5 @@ module.exports = {
     description: 'says pong',
     execute(message, bot){
         message.channel.send('pong');
-        var user = bot.users.find(user => user.id === message.author.id)
-        user.send('Good')
     }
 }
