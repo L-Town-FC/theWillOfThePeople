60b3e0d4a608efce9ce48f42b83644512258d883
Colin Krohto <ColinKrohto@gmail.com>
2021-01-24 17:01:38 -0500

    continued work on fish.js

--- commands/fish.js
+++ commands/fish.js
@@ -2,13 +2,24 @@ module.exports = {
     name: 'fish',
     description: 'lets you catch and track fish',
     execute(message, args, master, stats, tracker){
+        const fs = require('fs')
         var command = String(args[1]).toLowerCase() || 'none'
         const random = require('random')
+        fishes = JSON.parse(fs.readFileSync("./JSON/fishes.json"))
 
         switch(command){
             case 'cast':
                 var dist = random.normal(15,5)(0)
-                message.channel.send(`You caught a fish weighing ${dist.toFixed(2)} lbs`)
+                var index = Object.keys(fishes).length
+                var fish = Object.keys(fishes)[Math.floor(Math.random() * index)]
+                if(['a','e','i','o','u'].includes(fish[0].toLowerCase()) == true){
+                    article = 'an'
+                }else{
+                    article = 'a'
+                }
+
+                message.channel.send(`You caught ${article} ${fish} weighing ${dist.toFixed(2)} lbs`)
+                
             break
             case 'upgrade':
 
