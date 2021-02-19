d40d3566dd4a24dbf72204ae5948814abd171cbc
Colin Krohto <ColinKrohto@gmail.com>
2021-02-19 13:00:54 -0500

    worked on rarity generation and replacement

--- commands/fish.js
+++ commands/fish.js
@@ -15,12 +15,16 @@ module.exports = {
         if(typeof(user) =='undefined'){
             user = message.author.id
             profiles[user] = {
-                items: ['stick and bobber'],
+                rods: ['stick and bobber'],
+                rod: "stick and bobber",
                 boats: ["inflatable tube"],
                 boat: "inflatable tube",
-                rod: "stick and bobber",
+                baits: [],
                 bait: "",
+                tackles: [],
                 tackle: "",
+                misc: [],
+                extra_slot: "",
                 fishes:[],
                 biggest_fish:""
             }
@@ -84,27 +88,40 @@ function Fish(message, profiles, user){
     const fs = require('fs')
     var sizes = Size_Selector(profiles[user].boat)
     //After a size is chosen, the corresponding json is read. Then it randomly selects a fish from the list or junk
-    //Based on the rarity of the fish it decides if it should keep it or move on to the next fish
-    //ex. Epic rarity fish is first selected. 5% chance it keeps it. 95% chance it generates another fish
     var range = JSON.parse(fs.readFileSync(`./JSON/fish/${sizes}_fish.json`,'utf-8'))    
 
+    //json is turned into an array so it can be filtered
     var fish_array = []
     for(var i in range){
         fish_array.push(range[i])
     }
 
-    fish_array.filter(function(fish){
-        return fish.rarity == Rarity(profiles, user)
-    })
+    var filtered_fish = []
+    var excluded_list = []
 
-    var fish = fish_array[Math.floor(Math.random()*Object.keys(fish_array).length)]
+    while(filtered_fish.length == 0){
+        //a rarity is generated. A list of fish with that rarity is then made
+        //If no fish match that rarity, a new rarity is generated until a list can be made
+        var rarity = String(Rarity(profiles,user, excluded_list))
+        filtered_fish = fish_array.filter(function(fish){
+            return fish.rarity == rarity
+        })
+
+        if(filtered_fish.length == 0){
+            //makes sure failed rarites cant be seen again
+            excluded_list.push(rarity)
+        }
+    }
+
+    var fish = filtered_fish[Math.floor(Math.random()*Object.keys(filtered_fish).length)]
     var fish_weight = random.normal(fish.mean, fish.std)(0).toFixed(2)
     message.channel.send(`You caught a ${fish.name} weighing ${fish_weight} lbs`) 
-    console.log(fish)
 
-    function Rarity(profiles, user){
+    function Rarity(profiles, user, excluded){
+        //generates rarity based on different weightings
+        //users items can alter weightings
 
-        //always return common right now
+        //base values
         var C = 300
         var U = 150
         var R = 75
@@ -113,9 +130,36 @@ function Fish(message, profiles, user){
         var X = 7
         var G = 1
 
+        //setting eliminated values to 0
+        for(var i = 0; i < excluded.length; i++){
+            switch(excluded[i]){
+                case 'C':
+                    C = 0
+                break;
+                case 'U':
+                    U = 0
+                break;
+                case 'R':
+                    R = 0
+                break;
+                case 'E':
+                    E = 0
+                break;
+                case 'L':
+                    L = 0
+                break;
+                case 'X':
+                    X = 0
+                break;
+                case 'G':
+                    G = 0
+                break;
+            }
+        }
+
         var total = C + U + R + E + L + X + G
         var count = 0
-        for(var i = 0; i < 10000; i++){
+        //for(var i = 0; i < 10000; i++){
             var outcome = ""
             var chance = Math.random() * total
             if(chance <= G){
@@ -139,7 +183,7 @@ function Fish(message, profiles, user){
                 count++
             }
             */
-        }
+        //}
         //console.log(count * 100/10000)
         return outcome
     }
@@ -185,4 +229,9 @@ function Size_Selector(boat){
     } 
     var size = sizes[Math.floor(Math.random()*sizes.length)]
     return size
+}
+
+function Catch(profiles, user, rarity){
+    const fs = require('fs')
+
 }
