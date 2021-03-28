e3e95375a2da3cf83962f2d6aebb7651e3a722c7
Andrew Mollohan <atmollohan@gmail.com>
2021-03-28 17:08:00 -0400

    added secrets and deploy stuff

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,9 +10,9 @@ if [ ! -e "$env_file_location" ]; then
 else 
     echo "Sourcing .env for deployment it..."
     source env_file_location
-    echo "Ready to connect to $EC2_HOST as $AWS_USER"
+    echo "Ready to connect to $EC2_HOST as $EC2_USER"
     echo "Trial is $TRIAL on client"
-    ssh -i ~/.aws/my-ec2-key-pair.pem -o StrictHostKeyChecking=no $AWS_USER@$EC2_HOST  TRIAL=$TRIAL BOTTOKEN=$BOTTOKEN  IMAGE_NAME=$image_full_tag CONTAINER_NAME=$name '''
+    ssh -i ~/.aws/my-ec2-key-pair.pem -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST  TRIAL=$TRIAL BOTTOKEN=$BOTTOKEN  IMAGE_NAME=$image_full_tag CONTAINER_NAME=$name '''
     echo "Connected to $HOSTNAME as $USER at $PWD"
     echo "Trial is $TRIAL on server"
     docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME
