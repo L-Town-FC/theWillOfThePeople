1d36bea3dcfff643b7db74fa2a47b7df3c24ac6e
Andrew Mollohan <atmollohan@gmail.com>
2024-05-31 21:49:57 -0400

    add more scripts and github action

--- deploy/restartContainer.sh
+++ deploy/restartContainer.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+docker container inspect -f "{{.State.Status}}" $CONTAINER_NAME
 docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME
 docker pull $IMAGE_NAME
-docker run -d -e BOTTOKEN=$BOTTOKEN --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -e PRODBOTTOKEN=$PRODBOTTOKEN -e NODE_ENV=production -e FAUNA_KEY=$FAUNA_KEY --name $CONTAINER_NAME $IMAGE_NAME
