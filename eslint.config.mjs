8879a8e2cd5d5407b308e4d626e523dd5e874619
ColinKrohto <7willythekid@gmail.com>
2024-07-05 16:24:06 -0400

    updated eslint to include node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,16 @@ import pluginJs from "@eslint/js";
 
 // https://eslint.org/docs/latest/use/command-line-interface
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"], 
+    languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: 
+    { globals: 
+      globals.browser}
+  },{
+    languageOptions:{
+      globals: globals.node
+    }
+  },
   pluginJs.configs.recommended,
 ];
