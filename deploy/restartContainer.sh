8ee7fe92307cc7b2a53b6bd6f25b0ce75a89c1da
atmollohan <atmollohan@gmail.com>
2025-05-17 13:47:01 -0400

    remove fauna env vars from actions and scripts

--- deploy/restartContainer.sh
+++ deploy/restartContainer.sh
@@ -2,4 +2,4 @@
 docker container inspect -f "{{.State.Status}}" $CONTAINER_NAME
 docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME
 docker pull $IMAGE_NAME
-docker run -d -e PRODBOTTOKEN=$PRODBOTTOKEN -e NODE_ENV=production -e FAUNA_KEY=$FAUNA_KEY --name $CONTAINER_NAME $IMAGE_NAME
+docker run -d -e PRODBOTTOKEN=$PRODBOTTOKEN -e NODE_ENV=production --name $CONTAINER_NAME $IMAGE_NAME
